56e684588ebe245bb2b67be347aa173701e13ec9
AminulIslamorange <aminulislamorange@gmail.com>
2022-11-30 11:34:49 +0600

    first setup commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+
+app.use(cors())
+app.use(express.json())
