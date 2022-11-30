8274eb4bab923790de7065ac6d7b49eab7f9e632
AminulIslamorange <aminulislamorange@gmail.com>
2022-11-30 11:36:02 +0600

    mongo setup commit

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ const jwt = require('jsonwebtoken');
 
 app.use(cors())
 app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lg5fruz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
